0dfdf865093ee32e8b86078038a9b67b99964300
prachiuikey <52187509+prachiuikey@users.noreply.github.com>
2021-04-22 00:31:21 +0530

    QuerySelector && QuerySelectorALL

--- dom.js
+++ dom.js
@@ -1,43 +1,20 @@
-var itemList = document.querySelector("#items");
-// parentElement
-console.log(itemList.parentElement);
-itemList.parentElement.style.backgroundColor = '#f4f4f4';
 
-// Child
-console.log(itemList.children);
-console.log(itemList.childNodes);
-console.log(itemList.lastChild);
-console.log(itemList.lastElementChild);
-console.log(itemList.firstChild);
-console.log(itemList.firstElementChild);
+// QUERYSELECTOR
+var items = document.querySelector('.list-group-item');
+console.log(items);
+items.nextElementSibling.style.backgroundColor = 'green';
+var firstItems = document.querySelector('.list-group-item:nth-child(1)');
+firstItems.style.color = 'blue';
+var LastItems = document.querySelector('.list-group-item:nth-child(3)');
+LastItems.style.visibility = 'hidden';
 
-// Siblings
-console.log(itemList.nextSibling);
-console.log(itemList.nextElementSibling);
-console.log(itemList.previousSibling);
-console.log(itemList.previousElementSibling);
-itemList.previousElementSibling.style.color = 'green';
+// QUERYSELECTORALL
+var items = document.querySelectorAll('li');
+console.log(items);
+items[1].style.backgroundColor = 'green';
 
+var odd = document.querySelectorAll('li:nth-child(odd)');
 
-// createElement
-var newDiv = document.createElement('div');
-newDiv.className = 'hello';
-newDiv.id = 'hello1';
-// SetAttribute
-newDiv.setAttribute('title', 'Hello div');
-// create Text node
-var newDivText = document.createTextNode('HELLO');
-// Add text to div
-newDiv.appendChild(newDivText);
-
-// Add HELLO word before Item Lister
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
-console.log(newDiv);
-
-container.insertBefore(newDiv, h1);
-
-// Add HEllo word before Item 1 
-var item = document.querySelector('.list-group');
-const li = document.querySelector('ul li');
-item.insertBefore(newDiv,li);
+for(let i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = 'green';
+}
